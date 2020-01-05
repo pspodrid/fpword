2fd6dd1be3d476a06a606aa39d2980d3dafa6802
Student <example@example.com>
2020-01-05 14:04:51 -0800

    create all views and routes

--- app.rb
+++ app.rb
@@ -11,35 +11,43 @@ get('/') do
   erb(:words)
 end
 
-get('/word') do
+get('/words') do
   @words = Word.all
   erb(:words)
 end
 
-get('/word/new') do
+get('/words/new_word') do
   erb(:new_word)
 end
 
-get('/word/:id') do
-  "This route will show a specific album based on its ID. The value of ID here is #{params[:id]}."
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
 end
 
-post('/word') do
+post('/words') do
   name = params[:word_name]
   word = Word.new(name, nil)
   word.save()
-  @albums = Album.all()
+  @words = Word.all()
   erb(:words)
 end
 
-get('/word/:id/edit') do
-  "This will take us to a page with a form for updating an album with an ID of #{params[:id]}."
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
 end
 
-patch('/word/:id') do
-  "This route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:name])
+  @words = Word.all
+  erb(:words)
 end
 
-delete('/word/:id') do
-  "This route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
 end
